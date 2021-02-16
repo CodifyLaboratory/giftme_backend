a31e721b619fc47927ce406e5252d9e6004ca976
Kubat Usubaliev <tuubish@users.noreply.github.com>
2021-02-16 22:01:47 +0600

    + Add User registration + Add User login logout

--- giftme/models.py
+++ giftme/models.py
@@ -2,6 +2,14 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Test(models.Model):
+    name = models.CharField(verbose_name="Наименование", max_length=255)
+    birth = models.DateTimeField(verbose_name="Дата рождения", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
 class GiftMeUser(AbstractUser):
     email = models.EmailField(max_length=100, unique=True, verbose_name="Электронная почта")
     about = models.CharField(max_length=200, verbose_name="О себе")
